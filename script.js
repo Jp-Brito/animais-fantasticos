3feff130089340d3e527f6951d8419381a3fe812
João Pedro <jpbritoalm@gmail.com>
2023-02-07 13:21:12 -0300

    .

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log(window.location.href);
+
+let ativo = document.querySelector(".ativo");
+console.log(ativo);
+
+console.log(window.navigator.language);
+
+console.log(window.innerWidth, "por", window.innerHeight);
